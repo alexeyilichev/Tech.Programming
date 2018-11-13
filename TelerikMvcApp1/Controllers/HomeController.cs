a888ac98e279162548d756777e4edfc83d0309dc
Alexey Ilichev <alexey.ilichev@altenar.com>
2018-11-13 08:02:42 +0300

    Remove unused files and fix grid on index page

--- TelerikMvcApp1/Controllers/HomeController.cs
+++ TelerikMvcApp1/Controllers/HomeController.cs
@@ -11,24 +11,18 @@ namespace TelerikMvcApp1.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
             return View();
         }
         public ActionResult GetOrders()
         {
-            var result = new Order().Orders.Select(i => new Orders
-            {
-                Name = i.Name,
-                Count = i.Count,
-                Price = i.Price,
-                Place = i.Place
-            }
-            ).ToList();
-
-            return PartialView("_Orders", result);
+                return PartialView("_Orders");
+        }
+        public ActionResult Orders_Read(Orders model)
+        {
+                return PartialView("_Orders", model);
         }
     }
 }
--- TelerikMvcApp1/Models/Order.cs
+++ TelerikMvcApp1/Models/Order.cs
@@ -16,6 +16,7 @@ namespace TelerikMvcApp1.Models
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
